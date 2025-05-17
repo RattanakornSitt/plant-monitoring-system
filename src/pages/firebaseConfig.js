c9dff93850bd2eec8e4f25313ee310fff2582800
Rattanakorn Sittisang <rattanakorn.sitt@bumail.net>
2025-05-17 14:48:33 +0700

    update .env

--- src/pages/firebaseConfig.js
+++ src/pages/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByRSfKC2pRP5U5ulzswhGnTmk_19B5KTw",
-  authDomain: "project-plant-monitoring-b580a.firebaseapp.com",
-  projectId: "project-plant-monitoring-b580a",
-  storageBucket: "project-plant-monitoring-b580a.firebasestorage.app",
-  messagingSenderId: "512464485373",
-  appId: "1:512464485373:web:dd3792ebaf8a25e08f7794",
-  measurementId: "G-CDJBB3E3RK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
